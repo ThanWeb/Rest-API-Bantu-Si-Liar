7dcc407deef2775c675c9b93aac3afa6e8812603
Hans <classsickpeople@gmail.com>
2022-11-26 12:54:51 +0800

    Menambahkan query untuk create profile table dan insert data ke profile table

--- database/create.sql
+++ database/create.sql
@@ -21,6 +21,24 @@ INSERT INTO `tb_account` (
 
 -- CREATE PROFILE TABLE
 
+CREATE TABLE `db_bantu_si_liar`.`tb_profile` (
+    `id` INT NOT NULL AUTO_INCREMENT , 
+    `name` VARCHAR(60) NOT NULL , 
+    `province` VARCHAR(60) NOT NULL , 
+    `city` VARCHAR(100) NOT NULL , 
+    `address` VARCHAR(200) NOT NULL , 
+    `phone` VARCHAR(20) NOT NULL , 
+    `picture` VARCHAR(100) NOT NULL , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+-- INSERT DATA TO PROFILE TABLE
+
+INSERT INTO `tb_profile` (
+    `id`, `name`, `province`, `city`, `address`, `phone`, `picture`) 
+    VALUES (NULL, 'admin', 'Bali', 'Kabupaten Tabanan', 'Desa Pandak Gede, Kediri', '083115698773', 'https://img.icons8.com/color/48/null/circled-user-male-skin-type-4--v1.png'
+)
+
 -- CREATE REPORT TABLE
 
 -- CREATE ARTICLE TABLE
