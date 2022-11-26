a73fe52901563405721c334cbf5b48359877d7b2
Hans <classsickpeople@gmail.com>
2022-11-26 12:31:16 +0800

    Menambahkan query untuk create db, create account table dan insert data ke account table

--- database/create.sql
+++ database/create.sql
@@ -1,6 +1,23 @@
 -- CREATE DATABASE
 
+CREATE DATABASE `db_bantu_si_liar`;
+
 -- CREATE ACCOUNT TABLE
+
+CREATE TABLE `db_bantu_si_liar`.`tb_account` (
+    `id` INT NOT NULL AUTO_INCREMENT , 
+    `username` VARCHAR(30) NOT NULL , 
+    `email` VARCHAR(50) NOT NULL , 
+    `password` VARCHAR(50) NOT NULL , 
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+-- INSERT DATA TO ACCOUNT TABLE
+
+INSERT INTO `tb_account` (
+    `id`, `username`, `email`, `password`) 
+    VALUES (NULL, 'admin', 'hansrio@gmail.com', 'BantuLiarAdmin198'
+)
 
 -- CREATE PROFILE TABLE
 
