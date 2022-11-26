f89b3edb89ea7f7016e89f66d1a4c3a1bf84ad74
Hans <classsickpeople@gmail.com>
2022-11-26 15:45:38 +0800

    Menambahkan file untuk connect database

--- database/connect.php
+++ database/connect.php
@@ -1,14 +1,11 @@
 <?php 
     class Database {
-        private $host = "localhost";
-        private $database = "db_bantu_si_liar";
-        private $username = "root";
-        private $password = "";
-
+        private $host = "localhost", $database = "db_bantu_si_liar", $username = "root", $password = "";
         public $connect;
+
         public function connectDatabase () {
             try {
-                $this->connect = new PDO("mysql:host=".$this->host.";dbname=".$this->database,$this->username,$this->password);
+                $this->connect = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
                 $this->connect->exec("set names utf8");
             } catch (PDOException $exception) {
                 echo "Error: ".$exception->getMessage();
