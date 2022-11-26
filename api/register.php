80ee2f4c7d0d6537653101b78bc53913c8bb18d9
Hans <classsickpeople@gmail.com>
2022-11-26 15:49:02 +0800

    Menambahkan API untuk fitur register

---
+++ api/register.php
@@ -0,0 +1,21 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    include_once '../database/connect.php';
+    include_once '../function/accounts.php';
+
+    $database = new Database();
+    $connect = $database->connectDatabase();
+    $account = new Accounts($connect);
+    $data = json_decode(file_get_contents("php://input"));
+
+    $account->username = $data->username;
+    $account->email = $data->email;
+    $account->password = $data->password;
+
+    echo json_encode($account->createAccount());
+?>
