660a97a1edabad8681b7f9f429c4c0156066f255
Hans <classsickpeople@gmail.com>
2022-11-26 18:17:01 +0800

    Menghapus tabel profile

--- database/initialize.sql
+++ database/initialize.sql
@@ -9,20 +9,6 @@ CREATE TABLE `db_bantu_si_liar`.`tb_account` (
     `username` VARCHAR(30) NOT NULL , 
     `email` VARCHAR(50) NOT NULL , 
     `password` VARCHAR(50) NOT NULL , 
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
-
--- INSERT DATA TO ACCOUNT TABLE
-
-INSERT INTO `tb_account` (
-    `id`, `username`, `email`, `password`) 
-    VALUES (NULL, 'admin', 'hansrio@gmail.com', 'BantuLiarAdmin198'
-)
-
--- CREATE PROFILE TABLE
-
-CREATE TABLE `db_bantu_si_liar`.`tb_profile` (
-    `id` INT NOT NULL AUTO_INCREMENT , 
     `name` VARCHAR(60) NOT NULL , 
     `province` VARCHAR(60) NOT NULL , 
     `city` VARCHAR(100) NOT NULL , 
@@ -32,11 +18,11 @@ CREATE TABLE `db_bantu_si_liar`.`tb_profile` (
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
--- INSERT DATA TO PROFILE TABLE
+-- INSERT DATA TO ACCOUNT TABLE
 
-INSERT INTO `tb_profile` (
-    `id`, `name`, `province`, `city`, `address`, `phone`, `picture`) 
-    VALUES (NULL, 'admin', 'Bali', 'Kabupaten Tabanan', 'Desa Pandak Gede, Kediri', '083115698773', 'https://img.icons8.com/color/48/null/circled-user-male-skin-type-4--v1.png'
+INSERT INTO `tb_account` (
+    `id`, `username`, `email`, `password`, ) 
+    VALUES (NULL, 'admin', 'hansrio@gmail.com', 'BantuLiarAdmin198', 'Admin Bali', 'Bali', 'Kabupaten Tabanan', 'Desa Pandak Gede, Kediri', '083115698773', 'https://img.icons8.com/color/48/null/circled-user-male-skin-type-4--v1.png'
 )
 
 -- CREATE REPORT TABLE
