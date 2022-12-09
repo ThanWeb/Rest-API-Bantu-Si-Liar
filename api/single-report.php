bdec2785a0e14b8b45a995a49571ae0e914fd48a
Hans <classsickpeople@gmail.com>
2022-12-09 09:32:33 +0800

    Menambahkan API untuk load single artikel

---
+++ api/single-report.php
@@ -0,0 +1,17 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    include_once '../database/connect.php';
+    include_once '../function/articles.php';
+
+    $database = new Database();
+    $connect = $database->connectDatabase();
+    $report = new Reports($connect);
+    $report->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    echo json_encode($report->getSingleReport());
+?>g
